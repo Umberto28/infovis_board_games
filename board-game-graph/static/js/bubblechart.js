001df442dbd5c0b711bdf58557a57c0ff4525563
ricciric <riki.marasca@gmail.com>
2024-07-24 14:26:49 +0200

    Fixed bubble chart visualization

--- board-game-graph/static/js/bubblechart.js
+++ board-game-graph/static/js/bubblechart.js
@@ -1,9 +1,8 @@
-// Load data and create bubble chart
 d3.json("/data").then(data => {
     // Set the dimensions and margins of the graph
-    const margin = {top: 10, right: 30, bottom: 50, left: 60},
-          width = 800 - margin.left - margin.right,
-          height = 600 - margin.top - margin.bottom;
+    const margin = {top: 30, right: 200, bottom: 100, left: 100},
+          width = 1200 - margin.left - margin.right,  // Increased width
+          height = 800 - margin.top - margin.bottom;  // Increased height
 
     // Append the svg object to the body of the page
     const svg = d3.select("#bubblechart")
@@ -13,24 +12,31 @@ d3.json("/data").then(data => {
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    const tooltip = d3.select("#tooltip");
-    const card = d3.select("#card");
+    const tooltip = d3.select("#tooltip")
+        .style("opacity", 0);
+    const card = d3.select("#card")
+        .style("display", "none");
 
     // Compute the average players and prepare the data
     const processedData = d3.rollups(data, v => {
         return {
-            avgPlayers: Math.round(d3.mean(v, d => (d.minplayers + d.maxplayers) / 2)),
+            avgPlayers: d3.mean(v, d => (d.minplayers + d.maxplayers) / 2),
             avgRanking: d3.mean(v, d => d.rank),
-            games: v.map(d => ({ name: d.title, rank: d.rank }))
+            games: v.map(d => ({ name: d.title, rank: d.rank, minplayers: d.minplayers, maxplayers: d.maxplayers })),
         };
-    }, d => d.year, d => Math.round((d.minplayers + d.maxplayers) / 2)).map(([year, values]) => {
+    }, d => d.year, d => (d.minplayers + d.maxplayers) / 2).map(([year, values]) => {
         return values.map(([avgPlayers, metrics]) => ({
             year: year,
             avgPlayers: avgPlayers,
             avgRanking: metrics.avgRanking,
-            games: metrics.games
+            games: metrics.games,
+            numGames: metrics.games.length
         }));
     }).flat();
+
+    function float2int (value) {
+        return value | 0;
+    }
 
     // Extract unique years from the dataset
     const uniqueYears = [...new Set(processedData.map(d => d.year))].sort((a, b) => a - b);
@@ -51,7 +57,6 @@ d3.json("/data").then(data => {
     maxYearSlider.value = uniqueYears.length - 1;
     maxYearValue.textContent = uniqueYears[uniqueYears.length - 1];
 
-    // Function to create the bubble chart
     function createBubbleChart(filteredData) {
         svg.selectAll("*").remove();
 
@@ -63,31 +68,47 @@ d3.json("/data").then(data => {
             .attr("transform", `translate(0,${height})`)
             .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
+        // X axis label
+        svg.append("text")
+            .attr("text-anchor", "end")
+            .attr("x", width)
+            .attr("y", height + margin.top + 20)
+            .text("Release Year");
+
         // Add Y axis
         const y = d3.scaleLinear()
-            .domain([d3.min(filteredData, d => d.avgPlayers) - 1, d3.max(filteredData, d => d.avgPlayers) + 1])
+            .domain([0, 6]) // Y axis from 0 to 6 // Avg players on the y-axis
             .range([height, 0]);
         svg.append("g")
             .call(d3.axisLeft(y));
 
-        // Add a scale for bubble size (correct for ranking)
+        // Y axis label
+        svg.append("text")
+            .attr("text-anchor", "end")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -margin.left + 60)
+            .attr("x", -margin.top)
+            .text("Average Number of Players");
+
+        // Add a scale for bubble size based on number of games
         const z = d3.scaleLinear()
-            .domain(d3.extent(filteredData, d => d.avgRanking).reverse())
-            .range([4, 40]);
+            .domain(d3.extent(filteredData, d => d.numGames))
+            .range([4, 60]); // Adjusted to make size differences more pronounced
 
         // Add a color scale for ranking
         const color = d3.scaleSequential(d3.interpolateRdYlBu)
             .domain([5, 100]);  // Rank 1 will be red, Rank 100 will be blue
 
-        // Add bubbles
-        svg.append("g")
+        // Add bubbles with transitions
+        const bubbles = svg.append("g")
             .selectAll("circle")
-            .data(filteredData)
-            .enter()
+            .data(filteredData);
+
+        bubbles.enter()
             .append("circle")
             .attr("cx", d => x(d.year))
-            .attr("cy", d => y(d.avgPlayers))
-            .attr("r", d => z(d.avgRanking))
+            .attr("cy", d => y(d.avgPlayers))  // Y coordinate based on avgPlayers
+            .attr("r", 0) // Start with radius 0
             .style("fill", d => color(d.avgRanking))
             .style("opacity", "0.7")
             .attr("stroke", "black")
@@ -95,7 +116,7 @@ d3.json("/data").then(data => {
                 tooltip.transition()
                     .duration(200)
                     .style("opacity", .9);
-                tooltip.html(`Ranking: ${d.avgRanking.toFixed(2)}`)
+                tooltip.html(`Year: ${d.year}<br>Average Ranking: ${float2int(d.avgRanking.toFixed(2))}<br>Number of Board Games in this group: ${d.numGames}<br>Average Number of players: ${d.avgPlayers}`)
                     .style("left", (event.pageX + 5) + "px")
                     .style("top", (event.pageY - 28) + "px");
             })
@@ -109,12 +130,27 @@ d3.json("/data").then(data => {
                     .style("left", (event.pageX + 5) + "px")
                     .style("top", (event.pageY + 5) + "px")
                     .html(`
-                        <h4>Games from ${d.year} with ${d.avgPlayers} players</h4>
+                        <h4>Games from ${d.year} with ${d.avgPlayers} average number of players</h4>
                         <ul>
-                            ${d.games.map(game => `<li>${game.name} (Rank: ${game.rank})</li>`).join('')}
+                            ${d.games.map(game => `<li>${game.name} (Rank: ${game.rank}), (Players: ${game.minplayers}-${game.maxplayers})</li>`).join('')}
                         </ul>
                     `);
-            });
+            })
+            .transition()
+            .duration(1000)
+            .attr("r", d => z(d.numGames)); // Animate to final radius
+
+        bubbles.transition()
+            .duration(1000)
+            .attr("cx", d => x(d.year))
+            .attr("cy", d => y(d.avgPlayers))  // Y coordinate based on avgPlayers
+            .attr("r", d => z(d.numGames));
+
+        bubbles.exit()
+            .transition()
+            .duration(1000)
+            .attr("r", 0)
+            .remove();
 
         // Hide the card when clicking outside
         d3.select("body").on("click", function(event) {
@@ -122,6 +158,40 @@ d3.json("/data").then(data => {
                 card.style("display", "none");
             }
         });
+            
+        // Add legend for color
+        const legendColor = svg.append("g")
+            .attr("transform", `translate(${-margin.left + 5},${height / 2 + 200})`);
+    
+        legendColor.append("text")
+            .attr("x", 0)
+            .attr("y", -20)
+            .text("Ranking")
+            .style("font-size", "14px")
+            .attr("alignment-baseline", "middle");
+    
+        const colorValues = [5, 50, 100];
+    
+        legendColor.selectAll("rect")
+            .data(colorValues)
+            .enter()
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", (d, i) => i * 20)
+            .attr("width", 20)
+             .attr("height", 20)
+            .style("fill", d => color(d));
+    
+        legendColor.selectAll("text.color")
+            .data(colorValues)
+            .enter()
+            .append("text")
+            .attr("x", 30)
+            .attr("y", (d, i) => 10 + i * 20)
+            .text(d => d)
+            .style("font-size", "12px")
+            .attr("alignment-baseline", "middle");
+    
     }
 
     // Initial display
