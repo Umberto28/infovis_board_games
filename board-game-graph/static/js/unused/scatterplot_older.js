f6acde2bb846173696a943708c174b0e24661429
Eucleth <eucleth.contreras@gmail.com>
2024-07-27 17:31:27 +0200

    Updated scatterplot
    Added mousehover information, mouse click to get all the info of clicked game, option to order the rank in ascending or descending.

--- board-game-graph/static/js/unused/scatterplot_older.js
+++ board-game-graph/static/js/unused/scatterplot_older.js
@@ -1,19 +1,6 @@
 d3.json('/data').then(function(data) {
     const color = d3.scaleOrdinal().range(['#85edc3', '#e6a766', '#5288e4', '#e09793', '#af91df', '#dab974', '#53d55a', '#97f1a3', '#a7e290', '#e4d14f', '#e99561', '#3be8a7', '#9ae6b8', '#e6255c', '#d38664', '#7fdeed', '#b0dd9a', '#7097d3', '#4ac8d4', '#e9bcac', '#df9daa', '#e48e9c', '#948ce2', '#a5eedc', '#de938a', '#e04da1', '#dcca85', '#edd7a0', '#d2789f', '#c4ef9b', '#6fbae1', '#eae655', '#2edc3d', '#51c2e1', '#d14ca9', '#dd8470', '#c2ea4f', '#e05789', '#e3b96f', '#bb5ae8', '#475bc5', '#94ebc4', '#d264dc', '#3a33de', '#8972da', '#70e7e2', '#91e2d9', '#a4b3ef', '#cfee9d', '#e03e64', '#b29add', '#c691e6', '#93d77c', '#6a6fe4', '#e04c58', '#48b3d9', '#cd66c3', '#d151e9', '#e285b3', '#dc8eb8', '#9f62d1', '#5ace7c', '#dc67ea', '#dd2873', '#d8e4a1', '#79b4e9', '#abd059', '#59b1d6', '#f1a5e4', '#b537e8', '#a7e772', '#d9d16f', '#a9ebd9', '#cad581', '#c57a41', '#995cd5', '#d1d935', '#6bd762', '#3c4dd0', '#94eff0', '#e4e489', '#e063de', '#8887d9', '#aae6be', '#89d98a', '#69e8e0', '#de74e1', '#9ce192', '#87d798', '#a77ce2', '#cb79e5', '#3660e1', '#c6e48a', '#a6c2e4', '#e29771', '#9b8be8', '#72edac', '#b5ea90', '#77e563', '#50e6b5', '#e367c4', '#e6ce4c', '#59a2d8', '#ed8dd8', '#ad92ec', '#442cd4', '#9e46dd', '#e88c7e', '#c6944c', '#5bde92', '#ec5eb6', '#ddef99', '#cae5ab', '#81d5c4', '#436ec9', '#e19cde', '#e88b8b', '#7db1e6', '#e99f60', '#e26b6e', '#97e184', '#39dc64', '#48b0c5'])
 
-    const tooltip = d3.select('#tooltip');
-    const card = d3.select('#card');
-
-    const parameter_mappings = {
-        "year": "Year",
-        "minplayers": "Minimum Players",
-        "maxplayers": "Maximum Players",
-        "minplaytime": "Minimum Playtime",
-        "maxplaytime": "Maximum Playtime",
-        "avgplaytime": "Average Playtime",
-        "minage": "Minimum Age"
-    };
-
     const reviewCounts = data.map(d => d.rating.num_of_reviews).sort((a, b) => b - a);
     const reviewRanks = data.map(d => reviewCounts.indexOf(d.rating.num_of_reviews) + 1);
 
@@ -23,14 +10,14 @@ d3.json('/data').then(function(data) {
         game.popularity_rank = reviewRanks[data.indexOf(game)];
     });
 
-    const margin = { top: 50, right: 30, bottom: 50, left: 40 };
+    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
     const width = 960 - margin.left - margin.right;
     const height = 500 - margin.top - margin.bottom;
 
-    const barMargin = { top: 50, right: 5, bottom: 50, left: 10 };
+    const barMargin = { top: 20, right: 5, bottom: 40, left: 10 };
     const barWidth = 150;
 
-    const svg = d3.select("#scatterplot").append("svg")
+    const svg = d3.select("#scatterplot_old").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -72,31 +59,8 @@ d3.json('/data').then(function(data) {
     }
 
     // update scatterplot and bar chart
-    function updateAxes(x_parameter, y_parameter, x_order) {
-        svg.selectAll(".label").remove()
-
-        svg.append("text") // x-axis label
-            .attr("class", "label")
-            .attr("text-anchor", "end")
-            .attr("x", width)
-            .attr("y", height + margin.bottom - 15)
-            .text("Ranking by " + x_parameter);
-
-        svg.append("text") // y-axis label
-            .attr("class", "label")
-            .attr("text-anchor", "start")
-            .attr("x", -margin.left)
-            .attr("y", -10)
-            .text(parameter_mappings[y_parameter]);
-
-        // not really needed as the ranking domain is always 1 to 100
-        // const xDomain = x_parameter === 'popularity' ? [1, d3.max(data, d => d.popularity_rank)] : [1, d3.max(data, d => d.rank)];
-        const xDomain = [1, 100];
-
-        if (x_order === 'descending') {
-            xDomain.reverse();
-        }
-
+    function updateAxes(x_parameter, y_parameter) {
+        const xDomain = x_parameter === 'popularity' ? [1, d3.max(data, d => d.popularity_rank)] : [1, d3.max(data, d => d.rank)];
         x.domain(xDomain);
         x_axis.transition().call(d3.axisBottom(x));
 
@@ -160,39 +124,9 @@ d3.json('/data').then(function(data) {
             .attr("cy", d => y(String(d[y_parameter])) + y.bandwidth() / 2)
             .attr("r", 5)
             .attr("fill", d => colorScale(String(d[y_parameter])))
-            .on('mouseover', (event, d) => {
-                const game = data.find(game => game.id === d.id);
-                const rank = (x_parameter === 'popularity' ? d.popularity_rank : d.rank);
-                tooltip.classed('hidden', false)
-                    .html(game.title + ' (rating rank: ' + d.rank + ' | popularity rank: ' + d.popularity_rank + ')')
-                    .style('left', (event.pageX + 5) + 'px')
-                    .style('top', (event.pageY + 5) + 'px');
-            }).on('mouseout', () => {
-                tooltip.classed('hidden', true);
-            }).on('click', (event, d) => {
-                event.stopPropagation();
-                const game = data.find(game => game.id === d.id);
-                card.classed('hidden', false)
-                    .html(`
-                    <h2>${game.title}</h2>
-                    <p><strong>Rating Rank:</strong> ${game.rank}</p>
-                    <p><strong>Popularity Rank:</strong> ${d.popularity_rank}</p>
-                    <p><strong>Rating:</strong> ${game.rating.rating}</p>
-                    <p><strong>Reviews:</strong> ${game.rating.num_of_reviews}</p>
-                    <p><strong>Year:</strong> ${game.year}</p>
-                    <p><strong>Players:</strong> ${game.minplayers} - ${game.maxplayers}</p>
-                    <p><strong>Play Time:</strong> ${game.minplaytime} - ${game.maxplaytime} mins</p>
-                    <p><strong>Age:</strong> ${game.minage}+</p>
-                    <p><strong>Categories:</strong> ${game.types.categories.map(c => c.name).join(' | ')}</p>
-                    <p><strong>Mechanics:</strong> ${game.types.mechanics.map(m => m.name).join(' | ')}</p>
-                    <p><strong>Designer:</strong> ${game.credit.designer.map(d => d.name).join(' | ')}</p>
-                    `)
-                    .style('left', (event.pageX + 10) + 'px')
-                    .style('top', (event.pageY + 10) + 'px');
-                console.log('Card shown:', game);
-            })
             .merge(circles)
             .transition()
+            // .attr("cx", d => x(d.rank))
             .attr("cx", d => x(x_parameter === 'popularity' ? d.popularity_rank : d.rank))
             .attr("cy", d => y(String(d[y_parameter])) + y.bandwidth() / 2)
             .attr("fill", d => colorScale(String(d[y_parameter])));
@@ -231,37 +165,19 @@ d3.json('/data').then(function(data) {
         barYAxis.transition().call(d3.axisRight(barY).tickSize(0).tickFormat(''));
     }
 
-    document.addEventListener('click', () => {
-        card.classed('hidden', true);
-        console.log('Card hidden');
-    });
-    
-    // Prevent card from hiding when clicking on the card itself
-    card.on('click', (event) => {
-        event.stopPropagation();
-    });
-
     // default values
-    updateAxes("rating", "year", "descending");
+    updateAxes("rank", "year");
 
     // updates
     d3.select("#x-axis-select").on("change", function() {
         const xParameter = this.value;
         const yParameter = d3.select("#y-axis-select").node().value;
-        const xOrder = d3.select("#x-order-select").node().value;
-        updateAxes(xParameter, yParameter, xOrder);
+        updateAxes(xParameter, yParameter);
     });
     d3.select("#y-axis-select").on("change", function() {
         const xParameter = d3.select("#x-axis-select").node().value;
         const yParameter = this.value;
-        const xOrder = d3.select("#x-order-select").node().value;
-        updateAxes(xParameter, yParameter, xOrder);
-    });
-    d3.select("#x-order-select").on("change", function() {
-        const xParameter = d3.select("#x-axis-select").node().value;
-        const yParameter = d3.select("#y-axis-select").node().value;
-        const xOrder = this.value;
-        updateAxes(xParameter, yParameter, xOrder);
+        updateAxes(xParameter, yParameter);
     });
 }).catch(function(error) {
     console.error('Error fetching data:', error);
